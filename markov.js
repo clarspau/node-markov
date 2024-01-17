b81b1ec2b17857203a8c96c325d1867a9a7f0273
clarspau <125636195+clarspau@users.noreply.github.com>
2024-01-16 21:16:39 -0800

    created "makeChains()" method in markov.js for building the Markov chains based on the input text.

--- markov.js
+++ markov.js
@@ -16,8 +16,27 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
+
   makeChains() {
-    // TODO
+    // Initialize a Map to store the Markov chains
+    let chains = new Map();
+
+    // Iterate through the array of words
+    for (let i = 0; i < this.words.length; i += 1) {
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      // Check if the word is already in the chains
+      if (chains.has(word))
+        // If yes, add the next word to its existing array
+        chains.get(word).push(nextWord);
+      else
+        // If not, create a new entry with the current word and an array containing the next word
+        chains.set(word, [nextWord]);
+    }
+
+    // Save the generated chains in the class instance
+    this.chains = chains;
   }
 
 
